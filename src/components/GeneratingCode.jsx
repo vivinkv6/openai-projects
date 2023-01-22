68c9bda5745dff46795a16fe7b75652d9a849ceb
vivinkv6 <vivinkv0@gmail.com>
2023-01-22 09:15:54 +0530

    create sql translator

---
+++ src/components/GeneratingCode.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+
+function GeneratingCode() {
+
+    const [words, setWords] = useState("");
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateCode = async () => {
+    setCode("");
+    setLoading(true);
+    const response = await openai.createCompletion({
+      model: "code-davinci-002",
+      prompt: words,
+      temperature: 0,
+      max_tokens: 150,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["#", ";"],
+    });
+    setCode(response.data.choices[0].text);
+    setLoading(false);
+  };
+
+
+  return (
+   
+        <div className="col-md-12">
+          <center>
+            <textarea
+              cols="40"
+              rows="10"
+              onChange={(e) => setWords(e.target.value)}
+            ></textarea>
+          </center>
+          <center>
+            <button className="btn btn-primary mt-3" onClick={generateCode}>
+              Generate Code
+            </button>
+          </center>
+          {loading ? (
+            <>
+              <center className="text-light mt-5 mb-5"><h3>Generating Code</h3></center>
+              
+            </>
+          ) : (
+            <>
+            
+            </>
+            
+          )}
+          {code.length>0 ?<center>
+                <textarea className="mt-5" cols="40" rows="10" value={code}></textarea>
+              </center> : <></>}
+          
+        </div>
+     
+  )
+}
+
+export default GeneratingCode
--- src/pages/Sql.jsx
+++ src/pages/Sql.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import GeneratingCode from "../components/GeneratingCode";
+
 
 function Sql() {
+
+
   return (
-    <div className='container mt-5'>
-      <h1 className='text-light text-center'>Coming Soon</h1>
+    <div className="container mt-5">
+    <div className="row">
+      <GeneratingCode/>
     </div>
-  )
+    </div>
+  );
 }
 
-export default Sql
+export default Sql;
